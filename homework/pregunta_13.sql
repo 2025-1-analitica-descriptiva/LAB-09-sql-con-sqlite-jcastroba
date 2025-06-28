4fa689e89bfc37dbbee613be76acb5340f162550
Jose Manuel Castro Balbín <jcastroba@unal.edu.co>
2025-06-28 18:28:52 -0500

    Correcion Pregunta 13

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -1,8 +1,10 @@
 -- Pregunta 13: Calcular el promedio de c12 en tbl1 por K0,
--- pero solo para las filas con c13 ≥ 500, redondeado a dos decimales.
+-- pero solo para las filas con c13 ≥ 500, redondeado a dos decimales
 SELECT
   K0,
-  ROUND(AVG(c12), 2) AS "avg(c12)"
+  -- multiplica por 100, añade 0.5, trunca y divide para lograr
+  -- un redondeo “hacia arriba” en caso de .005 exactos
+  (CAST(AVG(c12) * 100.0 + 0.5 AS INTEGER)) / 100.0 AS "avg(c12)"
 FROM tbl1
 WHERE c13 >= 500
 GROUP BY K0
